eb41f30887f972b26151790d356c63faa21c18d5
David JULIEN <david.julien@gmail.com>
2020-10-05 09:15:17 +0200

    Add account model

---
+++ src/app/models/account.model.spec.ts
@@ -0,0 +1,26 @@
+import { Account, AccountOwnership, AccountType } from './account.model';
+
+describe('Account', () => {
+  it('should create an instance', () => {
+    const bankId = 'ing';
+    const clientId = 'client';
+    const accountId = 'account';
+    const balance = 123.12;
+    const number = 'number';
+    const owner = 'owner';
+    const ownership = AccountOwnership.SINGLE;
+    const type = AccountType.SAVINGS;
+    const name = 'name';
+
+    const account = new Account(bankId, clientId, accountId, balance, number, owner, ownership, type, name);
+    expect(account).toBeTruthy();
+    expect(account.bankId).toBe(bankId);
+    expect(account.clientId).toBe(clientId);
+    expect(account.balance).toBe(balance);
+    expect(account.number).toBe(number);
+    expect(account.owner).toBe(owner);
+    expect(account.ownership).toBe(ownership);
+    expect(account.type).toBe(type);
+    expect(account.name).toBe(name);
+  });
+});
---
+++ src/app/models/account.model.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from '../adapter';
+
+export enum AccountOwnership {
+  SINGLE = 0,
+    JOINT
+}
+
+export enum AccountType {
+  CURRENT = 0,
+    SAVINGS,
+    HOME_LOAN
+}
+
+export class Account {
+  constructor(
+    public readonly bankId: string,
+    public readonly clientId: string,
+    public readonly accountId: string,
+    public readonly balance: number,
+    public readonly number: string,
+    public readonly owner: string,
+    public readonly ownership: AccountOwnership,
+    public readonly type: AccountType,
+    public readonly name: string) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountAdapter implements Adapter<Account> {
+  adapt(item: any): Account {
+    return new Account(item.bank_id, item.client_id, item.id, item.balance, item.number, item.owner, 
+      AccountOwnership[item.ownership.toUpperCase() as keyof typeof AccountOwnership],
+      AccountType[item.type.toUpperCase() as keyof typeof AccountType],
+      item.name);
+  }
+}

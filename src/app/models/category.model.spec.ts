38e159c25f842af4f3f85d151d3c6227ab293483
David JULIEN <david.julien@gmail.com>
2020-11-27 15:38:16 +0100

    Add a function to sort 2 categories according to their position and name

--- src/app/models/category.model.spec.ts
+++ src/app/models/category.model.spec.ts
@@ -20,4 +20,51 @@ describe('Category', () => {
     expect(category2.upCategory).toBe(category1);
     expect(category2.level).toBe(2);
   });
+
+  it('should order categories correctly', () => {
+    const id1 = 1;
+    const name1 = 'Alimentation';
+    const category1 = new Category(id1, name1, null);
+
+    const id2 = 2;
+    const name2 = 'Un supermarché';
+    const category2 = new Category(id2, name2, category1);
+
+    const id3 = 3;
+    const name3 = 'Logement';
+    const category3 = new Category(id3, name3, null);
+
+    const id4 = 4;
+    const name4 = 'Taxes';
+    const category4 = new Category(id4, name4, category3);
+
+    const id5 = 5;
+    const name5 = 'Taxe habitation';
+    const category5 = new Category(id5, name5, category4);
+
+    const id6 = 6;
+    const name6 = 'Taxe foncière';
+    const category6 = new Category(id6, name6, category4);
+
+    expect(category1.compareTo(category1)).toBe(0);
+    expect(category1.compareTo(category2)).toBe(-1);
+    expect(category2.compareTo(category1)).toBe(1);
+
+    expect(category1.compareTo(category3)).toBe(-1);
+    expect(category3.compareTo(category1)).toBe(1);
+
+    expect(category2.compareTo(category3)).toBe(-1);
+    expect(category3.compareTo(category2)).toBe(1);
+
+    expect(category4.compareTo(category3)).toBe(1);
+    expect(category4.compareTo(category5)).toBe(-1);
+    expect(category5.compareTo(category4)).toBe(1);
+
+    expect(category2.compareTo(category5)).toBe(-1);
+    expect(category5.compareTo(category2)).toBe(1);
+
+    expect(category5.compareTo(category6)).toBe(1);
+    expect(category6.compareTo(category5)).toBe(-1);
+  });
+
 });
--- src/app/models/category.model.ts
+++ src/app/models/category.model.ts
@@ -10,4 +10,25 @@ export class Category {
       this.level = 1;
     }
   }
+
+  compareTo(category: Category): number {
+    if (this === category) {
+      return 0;
+    } else if (this.level < category.level) {
+      const up = this.compareTo(category.upCategory);
+      if (up === 0) {
+        return -1;
+      } else {
+        return up;
+      }
+    } else if (this.level > category.level) {
+      return - category.compareTo(this);
+    } else if (this.upCategory === null) {
+      return this.name.localeCompare(category.name);
+    } else if (this.upCategory.id === category.upCategory.id) {
+      return this.name.localeCompare(category.name);
+    } else {
+      return this.upCategory.compareTo(category.upCategory);
+    }
+  }
 }
